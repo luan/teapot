c61b2768232842bdf64934114e8a1aabd74231a8
Luan Santos <cfcluan@gmail.com>
2014-11-25 16:32:07 -0800

    Fix looks on the workstation list

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"strings"
 
@@ -26,26 +27,31 @@ func namify(guid string) string {
 	return strings.Replace(guid, "tiego-", "", 1)
 }
 
-func listWorkstations() []string {
-	// desiredLRPs, _ := client.DesiredLRPsByDomain(domain)
+type workstation struct {
+	Name        string
+	DockerImage string
+	State       string
+	Running     bool
+}
+
+func listWorkstations() []workstation {
+	desiredLRPs, _ := client.DesiredLRPsByDomain(domain)
 	actualLRPs, _ := client.ActualLRPsByDomain(domain)
 	lrps := []receptor.ActualLRPResponse{}
-	list := []string{}
+	list := []workstation{}
 
 	for _, lrp := range actualLRPs {
 		lrps = append(lrps, lrp)
-		name := namify(lrp.ProcessGuid)
-		list = append(list, name)
 	}
 
-	// for _, lrp := range desiredLRPs {
-	// 	// state := ""
-	// 	// if i := contains(lrps, lrp.ProcessGuid); i >= 0 {
-	// 	// 	state = lrps[i].State
-	// 	// }
-	// 	// name := namify(lrp.ProcessGuid)
-	// 	// list = append(list, name)
-	// }
+	for _, lrp := range desiredLRPs {
+		state := "STOPPED"
+		if i := contains(lrps, lrp.ProcessGuid); i >= 0 {
+			state = lrps[i].State
+		}
+		name := namify(lrp.ProcessGuid)
+		list = append(list, workstation{name, lrp.RootFSPath, state, state == "RUNNING"})
+	}
 
 	return list
 }
@@ -74,6 +80,13 @@ func main() {
 			Name      string
 			RouteRoot string
 		}{params["name"], routeRoot})
+	})
+
+	m.Get("/destroy/:name", func(params martini.Params, r render.Render) {
+		name := params["name"]
+		processGuid := fmt.Sprintf("%s-%s", domain, name)
+		client.DeleteDesiredLRP(processGuid)
+		r.HTML(200, "index", listWorkstations())
 	})
 
 	m.Use(martini.Static("public"))
